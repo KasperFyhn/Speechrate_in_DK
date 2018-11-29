a24ca150bed74516c922404e52c27061df5f33e3
Kasper Fyhn Jacobsen <kasperfyhnjacobsen@gmail.com>
2018-11-29 19:41:54 +0100

    updated stats (again), hopefully now it uploads properly

--- stats.R
+++ stats.R
@@ -1,8 +1,11 @@
 all.data <- read.csv(file.choose())
+levels(all.data$city) <- list(Skjern='SKERN', Bornholm='BORNHOLM', Nyborg='NYB',
+                              Sønderborg='SOEN', Næstved='NAESTV', København='KBH')
+levels(all.data$gender) <- list(Female='F', Male='M')
+all.data$speechtime = all.data$phon.time / all.data$dur
 
-## STOP - a possibility to run through with data trimmed for short turns as
-## these more often give very high numbers and a more spread distribution
-data <- subset(all.data, nsyll > 2 & dur >= 1)
+data <- subset(all.data, nsyll > 1 & dur > 2)
+
 
 ## means (trimmed) and standard deviations
 aggregate(data[, 'speechrate'], list(data$city),
@@ -39,7 +42,7 @@ null.model <- glmer(speechrate ~ 1 + (1|speaker/dyad),
                     )
 summary(null.model)
 
-data$popdensity = as.numeric(ordered(data$popdensity))
+data$popdensity = as.integer(ordered(data$popdensity))
 popd.model <- glmer(speechrate ~  popdensity + (1|speaker/dyad),
                     family=gaussian(link=log), data=data,
                     control = glmerControl(optimizer = "nloptwrap",
@@ -65,11 +68,142 @@ turn.model <- glmer(speechrate ~ turn.id + (1 + turn.id|speaker/dyad),
 summary(turn.model)
 anova(null.model, turn.model)
 
-full.model <- glmer(speechrate ~ log10(popdensity) + gender + turn.id
-                    + (1 + turn.id|speaker/dyad),
+full.model <- glmer(speechrate ~ log10(popdensity) + gender + (1|speaker/dyad),
                     family=gaussian(link=log), data = data,
                     control = glmerControl(optimizer = "nloptwrap",
                                            calc.derivs = FALSE)
                     )
 summary(full.model)
-anova(null.model, full.model)
+anova(gender.model, full.model) # test for popd
+anova(popd.model, full.model)   # test for gender
+
+## modeling the data - ARTICULATION RATE
+library(lme4)
+null.model <- glmer(articrate ~ 1 + (1|speaker/dyad),
+                    family=gaussian(link=log), data=data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(null.model)
+
+data$popdensity = as.integer(ordered(data$popdensity))
+popd.model <- glmer(articrate ~  popdensity + (1|speaker/dyad),
+                    family=gaussian(link=log), data=data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(popd.model)
+anova(null.model, popd.model)
+
+gender.model <- glmer(articrate ~ gender + (1|speaker/dyad),
+                      family=gaussian(link=log), data=data,
+                      control = glmerControl(optimizer = "nloptwrap",
+                                             calc.derivs = FALSE)
+)
+summary(gender.model)
+anova(null.model, gender.model)
+
+full.model <- glmer(articrate ~ log10(popdensity) + gender + (1|speaker/dyad),
+                    family=gaussian(link=log), data = data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(full.model)
+anova(gender.model, full.model) # test for popd
+anova(popd.model, full.model)   # test for gender
+
+## modeling the data - SPEECH TIME
+null.model <- glmer(speechtime ~ 1 + (1|speaker/dyad),
+                    family=gaussian(link=log), data=data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(null.model)
+
+data$popdensity = as.integer(ordered(data$popdensity))
+popd.model <- glmer(speechtime ~  popdensity + (1|speaker/dyad),
+                    family=gaussian(link=log), data=data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(popd.model)
+anova(null.model, popd.model)
+
+gender.model <- glmer(speechtime ~ gender + (1|speaker/dyad),
+                      family=gaussian(link=log), data=data,
+                      control = glmerControl(optimizer = "nloptwrap",
+                                             calc.derivs = FALSE)
+)
+summary(gender.model)
+anova(null.model, gender.model)
+
+full.model <- glmer(speechtime ~ log10(popdensity) + gender + (1|speaker/dyad),
+                    family=gaussian(link=log), data = data,
+                    control = glmerControl(optimizer = "nloptwrap",
+                                           calc.derivs = FALSE)
+)
+summary(full.model)
+anova(gender.model, full.model) # test for popd
+anova(popd.model, full.model)   # test for gender
+
+# nice plots for the poster
+library(ggplot2)
+tiff("gender.tiff", units="in", width=5, height=5, res=300)
+dev.off()
+
+# popd - SPEECH RATE
+ggplot(data, aes(x = reorder(city, popdensity), y = speechrate, fill=city)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Areas ordered by population density',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate in different areas')
+
+  
+# gender - SPEECH RATE
+ggplot(data, aes(x = gender, y = speechrate, fill=gender)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Gender',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate between genders')
+
+# popd - ARTIC RATE
+ggplot(data, aes(x = reorder(city, popdensity), y = articrate, fill=city)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Areas ordered by population density',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate in different areas')
+
+
+# gender - ARTIC RATE
+ggplot(data, aes(x = gender, y = articrate, fill=gender)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Gender',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate between genders')
+
+# popd - SPEECH TIME
+ggplot(data, aes(x = reorder(city, popdensity), y = speechtime, fill=city)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Areas ordered by population density',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate in different areas')
+
+
+# gender - SPEECH TIME
+ggplot(data, aes(x = gender, y = speechtime, fill=gender)) +
+  geom_violin() +
+  geom_boxplot(width = 0.5, outlier.size = 1, fill='white') +
+  theme(legend.position = 'none') +
+  labs(x = 'Gender',
+       y = 'Speech rate (syllables/sec)',
+       title = 'Speech rate between genders')
